7db8e1b6b6472b7b2746906ea51c63e4acd8bf73
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2017-04-04 22:43:07 +0200

    Test githook

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ app.listen(3000, function () {
 
 var url = 'mongodb://localhost:27017/test';
 MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
+  
   console.log("Connected correctly to server.");
   db.close();
 });
