c711a921d3897b1f153adc1faca8ecfacb8e1c2a
Ivan Annovazzi <ivan.annovazzi@gmail.com>
2017-04-04 22:18:40 +0200

    Test MongoDB

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var express = require('express')
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 var app = express()
 
 app.get('/', function (req, res) {
@@ -8,3 +10,11 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
   console.log('My app listening on port 3000!')
 })
+
+
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
